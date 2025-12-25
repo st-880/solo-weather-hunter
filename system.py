2390db018fc3cb13a9a9c62d92f0ccc52799e736
st-880 <sv-29@ukr.net>
2025-12-25 15:33:30 +0200

    feat: load API key from .env; add gitignore

--- system.py
+++ system.py
@@ -2,3 +2,20 @@ print("[SYSTEM] Weather Hunter Protocol Initiated.")
 
 city = input("Enter city name: ")
 print(f"Fetching weather for: {city}")
+
+import requests
+import os
+
+
+with open(".env") as f:
+    API_KEY = f.read().strip().split("=")[1]
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f"Weather in {city}:{temp}°C, {desc}")
+else:
+    print("❌ Error: City not found or invalid API key")
